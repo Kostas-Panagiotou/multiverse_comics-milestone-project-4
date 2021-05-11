715f47b9ae49ff575671005a83274da7856cefb4
kostas.panagiotou <kostas.panagiotou08@gmail.com>
2021-05-11 11:51:20 +0000

    Create a shopping bag app and added urls and template

---
+++ bag/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def view_bag(request):
+    """ A view that renders the bag contents page """
+
+    return render(request, 'bag/bag.html')
