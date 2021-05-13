69cae6310994f1b9d47c97f13c1ba97169ad47d4
kostas.panagiotou <kostas.panagiotou08@gmail.com>
2021-05-13 17:35:51 +0000

    By mistake i did not commit this change, i think i need a break- actually commit is Added forms, admin and signal

--- checkout/apps.py
+++ checkout/apps.py
@@ -2,5 +2,7 @@ from django.apps import AppConfig
 
 
 class CheckoutConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'checkout'
+
+    def ready(self):
+        import checkout.signals
