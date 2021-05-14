e3694cad21d796a5e080e087021244771d00c0d0
kostas.panagiotou <kostas.panagiotou08@gmail.com>
2021-05-14 10:23:33 +0000

    Create a checkout function on the views.py

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from .forms import OrderForm
+
+
+def checkout(request):
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.error(request,
+                       "There's nothing in your bag at this multiverse")
+        return redirect(reverse('products'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
