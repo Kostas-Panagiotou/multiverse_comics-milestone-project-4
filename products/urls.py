eb00f79228dd028ea42770f6e021afe4c83f6026
kostas.panagiotou <kostas.panagiotou08@gmail.com>
2021-05-20 17:10:10 +0000

    Added the new urls on the product/add, product/edit

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
-    path('<product_id>', views.product_detail, name='product_detail'),
+    path('<int:product_id>/', views.product_detail, name='product_detail'),
+    path('add/', views.add_product, name='add_product'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
 ]
