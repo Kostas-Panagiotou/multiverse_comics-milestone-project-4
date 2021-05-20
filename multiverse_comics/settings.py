44e789086970dd0b48bd4d2ecbeb0403212b9a2f
kostas.panagiotou <kostas.panagiotou08@gmail.com>
2021-05-20 11:01:35 +0000

    Added in the settings.py in the stripe section a default email for the site

--- multiverse_comics/settings.py
+++ multiverse_comics/settings.py
@@ -183,3 +183,4 @@ STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+DEFAULT_FROM_EMAIL = 'multiversecomics@example.com'
