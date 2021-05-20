b098164aa37ef92e3931b056bcfe7f0389dcf6fc
kostas.panagiotou <kostas.panagiotou08@gmail.com>
2021-05-20 18:19:55 +0000

    Create a new function delete product for the admin user, add urls

--- products/views.py
+++ products/views.py
@@ -76,11 +76,8 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-            messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
-        else:
-            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+            product = form.save()
+            return redirect(reverse('product_detail', args=[product.id]))
     else:
         form = ProductForm()
 
@@ -111,3 +108,10 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """ Delete a product from the store """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    return redirect(reverse('products'))
