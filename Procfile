bf74047e89a61eedea88d185497e4db8891c89ac
kostas.panagiotou <kostas.panagiotou08@gmail.com>
2021-05-24 18:48:42 +0000

    Create a procfile, populate the database with db.json, add gunicorn and start deployment to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn multiverse_comics.wsgi:application
--- multiverse_comics/settings.py
+++ multiverse_comics/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-+$k+7epoo%mh^fdtb+-a+y0kas46f@*wc2-i_a#h#q8ku$-^wg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['multiverse-comics.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -120,13 +121,17 @@ WSGI_APPLICATION = 'multiverse_comics.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
