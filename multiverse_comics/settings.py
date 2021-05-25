68917c9b2b6d62c8886f82bc0d5d71d625cd7d1e
kostas.panagiotou <kostas.panagiotou08@gmail.com>
2021-05-25 10:56:27 +0000

    Removed secret key and set debug

--- multiverse_comics/settings.py
+++ multiverse_comics/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+$k+7epoo%mh^fdtb+-a+y0kas46f@*wc2-i_a#h#q8ku$-^wg'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['multiverse-comics.herokuapp.com', 'localhost']
 
