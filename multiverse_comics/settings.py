6016b874c586fc8fcaee5ee2b2007e5914f2ca30
kostas.panagiotou <kostas.panagiotou08@gmail.com>
2021-05-25 12:15:17 +0000

    Change the secret key to be visible for now

--- multiverse_comics/settings.py
+++ multiverse_comics/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
-
+SECRET_KEY = 'q(!zl&c4+w)j!b7bslik26mcf=3=qp3=q)^)yr$7sk1+=ap43a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
