4dd469472d92f3eea257d64aac78691b6a3fd6cd
kostas.panagiotou <kostas.panagiotou08@gmail.com>
2021-05-27 13:57:35 +0000

    Change the card element on js with hidepostal code to true, to hide the american visa cards

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -24,7 +24,7 @@ var style = {
         iconColor: '#dc3545'
     }
 };
-var card = elements.create('card', {style: style});
+var card = elements.create('card', {style: style, hidePostalCode: true});
 card.mount('#card-element');
 
 // Handle realtime validation errors on the card element
