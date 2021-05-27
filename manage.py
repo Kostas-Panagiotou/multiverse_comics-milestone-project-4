8852bdad9e1e510e4e0957d58b4c9e23d0d586bd
kostas.panagiotou <kostas.panagiotou08@gmail.com>
2021-05-27 12:10:41 +0000

    Fix the bug in the settings, and in the manage.py

--- manage.py
+++ manage.py
@@ -1,13 +1,10 @@
-#!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
 
 
 def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                          'multiverse_comics.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'multiverse_comics.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
