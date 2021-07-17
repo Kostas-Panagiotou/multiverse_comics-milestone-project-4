4b29f1bf1bd6f871c3cbb0710572fe3dee7f9c10
kostas.panagiotou <kostas.panagiotou08@gmail.com>
2021-07-17 17:09:55 +0000

    Change the secret visibility to get through the environment

--- multiverse_comics/settings.py
+++ multiverse_comics/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+if os.path.exists("env.py"):
+    import env
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q(!zl&c4+w)j!b7bslik26mcf=3=qp3=q)^)yr$7sk1+=ap43a'
+SECRET_KEY =  os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
