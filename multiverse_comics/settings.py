d116f815be030ab9b26c23cedf55e8b7197cedf0
kostas.panagiotou <kostas.panagiotou08@gmail.com>
2022-11-27 14:34:21 +0000

    Create a new file build.sh and add on settings.py render external hostname

--- multiverse_comics/settings.py
+++ multiverse_comics/settings.py
@@ -29,6 +29,10 @@ SECRET_KEY =  os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+ if RENDER_EXTERNAL_HOSTNAME:
+   ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 ALLOWED_HOSTS = ['multiverse-comics.herokuapp.com', 'localhost']
 
 
